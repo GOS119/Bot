933eb09cbb5b6ef8383bfd98a85e41e69a5e2702
GOS119 <csc23374982@central.edu.gh>
2025-10-26 06:22:51 +0530

    Update ATAConfig_.py

--- ATASmileApi-main/ATAConfig_.py
+++ ATASmileApi-main/ATAConfig_.py
@@ -1,8 +1,8 @@
 # rename ATAConfig_.py to ATAConfig.py
 
-SMILE_UID = ""              # Merchant UserID
-SMILE_EMAIL = ""            # Merchant Email
-SMILE_KEY  = ""             # Merchant Key
+SMILE_UID = "2956869"              # Merchant UserID
+SMILE_EMAIL = "smileone042@gmail.com"            # Merchant Email
+SMILE_KEY  = "5c838eedbc05bb4a225bb1293581f97f"             # Merchant Key
 
 SMILE_URL = "https://www.smile.one"
 SMILE_API = {
